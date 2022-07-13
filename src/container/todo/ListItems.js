9315427d355cf008b36292d8e54a407aabb5f554
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-13 19:39:44 +0530

    Todo with edit and delete functionality in class component

--- src/container/todo/ListItems.js
+++ src/container/todo/ListItems.js
@@ -1,20 +1,27 @@
+import React, { Component } from 'react'
 
-import React from 'react'
-
-function ListItems(props) {
-    const items = props.items;
-    const listItems = items.map(e => {
+class ListItems extends Component {
+    constructor(props) {
+      super(props)
+    
+      const items = this.props.items;
+    }
+    
+  render() {
+    const listItems = this.props.items.map(e => {
         return(
             <div className='list' key={e.key}>
-                <input value={e.text} id={e.key} type="text" onChange={(event) => {props.setUpdate(event.target.value, e.key)}}/>
-                {/* <button onClick={(event) => {props.setUpdate(event.target.value, e.key)}}>Edit</button> */}
-                <button onClick={() => props.deleteItem(e.key)}>Del</button>
+                <input value={e.text} id={e.key} type="text" onChange={(event) => {this.props.setUpdate(event.target.value, e.key)}}/>
+                {/* <button onClick={(event) => {this.props.setUpdate(event.target.value, e.key)}}>Edit</button> */}
+                <button onClick={() => this.props.deleteItem(e.key)}>Del</button>
             </div>
         )
     })
-  return (
-      <div>{listItems}</div>
-  )
+    return (
+        <div>{listItems}</div>
+    )
+  }
 }
 
 export default ListItems
+
